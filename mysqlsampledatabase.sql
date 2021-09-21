d7aafc8eec57575bab90b36738487ba43ed37245
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-09-21 15:55:18 +0700

    second commit

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -7974,3 +7974,21 @@ CALL SetCounter(@counter,5); -- 8
 
 SELECT @counter; -- 8
 
+CREATE VIEW customer_views AS
+
+SELECT customerNumber, customerName, phone
+
+FROM  customers;
+
+select * from customer_views;
+
+CREATE OR REPLACE VIEW customer_views AS
+
+SELECT customerNumber, customerName, contactFirstName, contactLastName, phone
+
+FROM customers
+
+WHERE city = 'Nantes';
+
+DROP VIEW customer_views;
+
